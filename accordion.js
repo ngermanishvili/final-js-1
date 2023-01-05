838c156e78fd74751d3c0fada0967a36d80abd1d
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-01-06 02:15:59 +0400

    js-recon

---
+++ accordion.js
@@ -0,0 +1,10 @@
+
+const accordionTitles = document.querySelectorAll('.accordion-title');
+
+accordionTitles.forEach(title => {
+  title.addEventListener('click', () => {
+    title.classList.toggle('active');
+  });
+});
+
+
